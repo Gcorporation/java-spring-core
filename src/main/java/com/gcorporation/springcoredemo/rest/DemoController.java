74eb0d440fffffbe5d3d5e37a99f817d9347b162
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-06-11 10:36:40 +0530

    Qualifiers

--- src/main/java/com/gcorporation/springcoredemo/rest/DemoController.java
+++ src/main/java/com/gcorporation/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.gcorporation.springcoredemo.rest;
 
 import com.gcorporation.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public void setCoach(@Qualifier("baseballCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
